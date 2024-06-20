3c676e631f6a1cd3097a272b25ed3d0583cbd682
zJunioJz <junioj253@gmail.com>
2024-06-20 09:49:11 -0300

    definicao de novas funcoes e implementacao do while

--- MediaEstoque.cpp
+++ MediaEstoque.cpp
@@ -1,78 +1,91 @@
 #include <iostream>
-#include <locale.h>
 #include <iomanip>
 #include <math.h>
 
 using namespace std;
 
-float mediaEstoq(float val1, float val2, float val3)
+float mediaEstoque(float val1, float val2, float val3)
 {
-	float media;
-	media = (val1 + val2 + val3)/3;
-	return media;
+    float media;
+    media = (val1 + val2 + val3) / 3;
+    return media;
 }
 
-int main()
+void calcularPorcentagem(float val1, float val2, float val3, float porcentagem)
 {
-	setlocale(LC_ALL, "portuguese");
-	int opcao;
-	float val1, val2, val3, resultInicial, porcentage, resultFinal;
-	
-	for(int i=0; i<=30 ;i++)
-	{
+    float resultInicial, resultFinal;
 
-	cout << fixed << setprecision(2);
-	cout << "Informe o primeiro Valor..:";
-	cin >> val1;
-	cout << "Informe o segundo Valor..:";
-	cin >> val2;
-	cout << "Informe o terceiro Valor..:";
-	cin >> val3;
-	cout << "\nDeseja Calcular a Porcentagem?\n\n";
-	cout << "1: Porcentagem Acréscimo.\n";
-	cout << "2: Porcentagem Diminutiva.\n";
-	cout << "3: Exibir somente a média.\n";
-	cout << "\nInforme a Opção..:";
-	cin >> opcao;
-	
-	switch(opcao)
-	{
-		case 1:
-		    cout << "Informe a Porcentagem..:";
-			cin >> porcentage;
-			resultInicial = mediaEstoq(val1, val2, val3);
-			porcentage = porcentage/100;
-            resultFinal = round(resultInicial+(round(resultInicial)* porcentage));
-	        cout << "\nO Resultado da divisão..:" << resultInicial;
-	        cout << "\nO Resultado do Valor arredondado..:" << round(resultInicial);
-			cout << "\n\nO Resultado com Acréscimo da porcentagem..:" << round(resultFinal);
-			break;
-			
-		case 2:
-			cout << "Informe a Porcentagem..:";
-			cin >> porcentage;
-			resultInicial = mediaEstoq(val1, val2, val3);
-            porcentage = porcentage/100;
-            resultFinal = round(resultInicial-(round(resultInicial)* porcentage));
-            cout << "\nO Resultado da divisão..:" << resultInicial;
-	        cout << "\nO Resultado do Valor arredondado..:" << round(resultInicial);
-			cout << "\n\nO Resultado com a Subtração da porcentagem..:" << round(resultFinal);
-			break;
-			
-		case 3:
-			resultInicial = mediaEstoq(val1, val2, val3);
-			cout << "\nO Resultado da divisão..:" << resultInicial;
-	        cout << "\nO Resultado do Valor arredondado..:" << round(resultInicial);
-	        break;
-	        
-		default:
-	    	cout <<"Opcões Invalidas. Tente outra vez.";
-	    	
-	}
-	cout << "\n";
-	cout << "\n-------------------------------------------------\n";
-	cout << "\n";
-	
+    resultInicial = mediaEstoque(val1, val2, val3);
+    porcentagem = porcentagem / 100;
+
+    resultFinal = round(resultInicial + (round(resultInicial) * porcentagem));
+    cout << "\nO Resultado da divisao..:" << resultInicial;
+    cout << "\nO Resultado do Valor arredondado..:" << round(resultInicial);
+    cout << "\n\nO Resultado com Acrescimo da porcentagem..:" << round(resultFinal);
 }
-	return 0;
+
+void exibirMedia(float val1, float val2, float val3)
+{
+    float resultInicial;
+
+    resultInicial = mediaEstoque(val1, val2, val3);
+    cout << "\nO Resultado da divisao..:" << resultInicial;
+    cout << "\nO Resultado do Valor arredondado..:" << round(resultInicial);
+}
+
+int main(int argc, char const *args[])
+{
+    int opcao;
+    float val1, val2, val3, porcentagem;
+
+    while (true)
+    {
+        cout << fixed << setprecision(2);
+        cout << "Bem-vindo a calculadora de produtos contida no Estoque.\n\n";
+        cout << "Informe o primeiro Valor..:";
+        cin >> val1;
+        cout << "Informe o segundo Valor..:";
+        cin >> val2;
+        cout << "Informe o terceiro Valor..:";
+        cin >> val3;
+        cout << "\nDeseja Calcular a Porcentagem?\n\n";
+        cout << "1: Porcentagem Acrescimo.\n";
+        cout << "2: Porcentagem Diminutiva.\n";
+        cout << "3: Exibir somente a media.\n";
+        cout << "4: Encerrar o Programa.\n";
+        cout << "\nInforme a Opcao..:";
+        cin >> opcao;
+
+        if (opcao == 4)
+        {
+            cout << "Programa encerrado!";
+            break;
+        }
+
+        switch (opcao)
+        {
+        case 1:
+            cout << "Informe a Porcentagem..:";
+            cin >> porcentagem;
+            calcularPorcentagem(val1, val2, val3, porcentagem);
+            break;
+
+        case 2:
+            cout << "Informe a Porcentagem..:";
+            cin >> porcentagem;
+            calcularPorcentagem(val1, val2, val3, -porcentagem);
+            break;
+
+        case 3:
+            exibirMedia(val1, val2, val3);
+            break;
+
+        default:
+            cout << "Opcoes Invalidas. Tente outra vez.";
+        }
+        cout << "\n";
+        cout << "\n-------------------------------------------------\n";
+        cout << "\n";
+    }
+    return 0;
 }
